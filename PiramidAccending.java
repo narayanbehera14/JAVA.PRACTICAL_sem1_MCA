49274a9fbbe43d765c70a1a1c8f04f49da92e538
lijubehera <beheranarayancs222330@gmail.com>
2025-09-07 16:09:58 +0530

    Initial commit

---
+++ PiramidAccending.java
@@ -0,0 +1,9 @@
+public class PiramidAccending {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+    }
+}
