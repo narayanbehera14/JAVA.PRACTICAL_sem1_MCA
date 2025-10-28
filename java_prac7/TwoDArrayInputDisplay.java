2ead50efe28f667635cdf2b24c8737d56d6f3b82
narayanbehera14 <beheranarayancs222330@gmail.com>
2025-10-28 15:31:31 +0530

    updated

---
+++ java_prac7/TwoDArrayInputDisplay.java
@@ -0,0 +1,34 @@
+package java_prac7;
+
+import java.util.Scanner;
+
+public class TwoDArrayInputDisplay {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = scanner.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int cols = scanner.nextInt();
+
+        int[][] arr = new int[rows][cols];
+
+        System.out.println("Enter elements of the array:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                arr[i][j] = scanner.nextInt();
+            }
+        }
+
+        System.out.println("The 2D array is:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
